07fa4f32a15cf220729038a851ccc95cd01eafbd
Kim Eduard Saludes <saludeskimdev@gmai.com>
2024-02-12 17:48:58 +0800

    Add cmds

---
+++ script/commands/gmeet.js
@@ -0,0 +1,37 @@
+module.exports = {
+    config: {
+        name: "gmeet",
+        alias: ["gmeet"],
+        version: 1.0,
+        author: "Saludes, Kim Eduard",
+        role: 1,
+        countdown: 0,
+        description: "Forward Gmeet link to the group",
+        category: "school",
+        guide: ""
+    },
+    onRun: async function () {},
+    onMessage: async function ({ api, event }) {
+        const thread_to_listen = [
+            "7379943615401950"
+        ];
+        if (event.body.includes('meet.google.com/') && thread_to_listen.includes(event.threadID)) {
+            const threadInfo = await api.getThreadInfo(event.threadID);
+            const threadName = threadInfo.threadName;
+            const match = event.body.match(/meet\.google\.com\/([\w-]+)/);
+
+            if (match && match[1]) {
+                const meetCode = match[1];
+                const meetLink = `https://meet.google.com/${meetCode}`;
+                const message = await api.sendMessage('A Google Meet link has been identified; the link is currently being forwarded to the students of BSIT-1B.', event.threadID);
+                await api.sendMessage(`From: ${threadName}\n===================\nGoogle Meet Code: ${meetCode}\nGoogle Meet Link: ${meetLink}`, event.senderID);
+                await delay(5000);
+                return api.editMessage('Students have been notified ✅', message.messageID);
+            }
+        }
+
+        function delay(ms) {
+            return new Promise(resolve => setTimeout(resolve, ms));
+        }
+    }
+};
---
+++ script/commands/notifyadmin.js
@@ -0,0 +1,29 @@
+module.exports = {
+    config: {
+        name: "notify_admin",
+        alias: "",
+        version: 1.0,
+        author: "Saludes, Kim Eduard",
+        role: 1,
+        countdown: 0,
+        description: "Notify admin",
+        category: "group",
+        guide: ""
+    },
+    onRun: async function () {},
+    onEvent: async function ({ api, event }) {
+		if (event.logMessageType === "log:subscribe") {
+            if (event.logMessageData.addedParticipants[0].userFbId === api.getCurrentUserID()) {
+                const threadInfo = await api.getThreadInfo(event.threadID);
+                const threadName = threadInfo.threadName;
+                const userInfo = await api.getUserInfo(event.author);
+                const msg = `Event: Added to a new group.\nGroup: ${threadName}\nAdded by: ${userInfo[event.author].name}`;
+                const fs = require("fs").promises;
+                const admin = await fs.readFile('config.json', "utf8");
+                JSON.parse(admin).admin.forEach(async (admin) => {
+                    await api.sendMessage(msg, admin);
+                });
+            }
+        }
+    }
+};
