2f4766b8cf2420199a96996db04e6a16665de015
Kim Eduard Saludes <saludeskimdev@gmai.com>
2024-02-12 23:18:36 +0800

    Change const to var

--- bot/system/load.js
+++ bot/system/load.js
@@ -1,12 +1,12 @@
 const fs = require('fs');
 const path = require('path');
 
-const loadedCommands = new Array();
+var loadedCommands = new Array();
 const loadedCommandNames = new Set();
 const loadedCommandAliases = new Set();
 
-const loadFriends = new Set();
-const loadConfig = new Array();
+var loadFriends = new Set();
+var loadConfig = new Array();
 
 function load_config() {
     const config = fs.readFileSync('config.json', "utf8");
