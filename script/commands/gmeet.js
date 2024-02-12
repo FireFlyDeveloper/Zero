c8bb6a58d94473e7f925c230b7acccf8b4b0bdfc
Kim Eduard Saludes <saludeskimdev@gmai.com>
2024-02-12 19:07:07 +0800

    Minor fix

--- script/commands/gmeet.js
+++ script/commands/gmeet.js
@@ -13,8 +13,11 @@ module.exports = {
     onRun: async function () {},
     onMessage: async function ({ api, event }) {
         const thread_to_listen = [
-            "7379943615401950" // Change this to target Group chat to listen for google meet link.
+            "7379943615401950" // Change this to target Group chat or chat to listen for google meet link.
         ];
+        const thread_to_send_notification = [
+            "7379943615401950" // Change this to target group chat or chat to send the notification
+        ]
         if (event.body.includes('meet.google.com/') && thread_to_listen.includes(event.threadID)) {
             const threadInfo = await api.getThreadInfo(event.threadID);
             const threadName = threadInfo.threadName;
@@ -24,7 +27,9 @@ module.exports = {
                 const meetCode = match[1];
                 const meetLink = `https://meet.google.com/${meetCode}`;
                 const message = await api.sendMessage('A Google Meet link has been identified; the link is currently being forwarded to the students of BSIT-1B.', event.threadID);
-                await api.sendMessage(`From: ${threadName}\n===================\nGoogle Meet Code: ${meetCode}\nGoogle Meet Link: ${meetLink}`, event.senderID);
+                thread_to_send_notification.forEach(async (threadID) => {
+                    await api.sendMessage(`From: ${threadName}\n===================\nGoogle Meet Code: ${meetCode}\nGoogle Meet Link: ${meetLink}`, threadID);
+                });
                 await delay(5000);
                 return api.editMessage('Students have been notified ✅', message.messageID);
             }
