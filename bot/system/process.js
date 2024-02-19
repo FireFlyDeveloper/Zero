1a1c18d62b8305439c09b969473873ed9e93ec0e
Kim Eduard Saludes <saludeskimdev@gmai.com>
2024-02-19 15:40:22 +0800

    Improve performance

--- bot/system/process.js
+++ bot/system/process.js
@@ -8,7 +8,6 @@ async function system_handler({ api, event }) {
 
         case "message":
             const args = event.body.split(' ');
-<<<<<<< HEAD
             onMessage({ api, event });
 
             if (!args[0].startsWith(global.utils.loadConfig[0].prefix)) return;
@@ -23,15 +22,6 @@ async function system_handler({ api, event }) {
             args[0] = args[0].slice(global.utils.loadConfig[0].prefix.length);
             
             const commandToRun = global.utils.loadedCommands.find(command => {
-=======
-            if (loadConfig[0].friend_only && !Array.from(loadFriends).some(friend => event.senderID === friend.userID)) return;
-            if (!loadConfig[0].group_thread && event.senderID !== event.threadID) return;
-            if (!loadConfig[0].personal_thread && event.senderID === event.threadID) return;
-            onMessage({ api, event });
-            if (!args[0].includes(loadConfig[0].prefix)) return;
-            args[0] = args[0].replace(loadConfig[0].prefix, '');
-            const commandToRun = loadedCommands.find(command => {
->>>>>>> 3f771dd4f9b42ae6f9bbb79916f8d99b8d7d5e50
                 return (
                     args[0].toLowerCase() === command.config.name ||
                     (command.config.alias && command.config.alias.includes(args[0].toLowerCase()))
