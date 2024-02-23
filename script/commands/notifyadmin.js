b5349780160154821f7c9390fa641775a56fc3b1
Kim Eduard Saludes <saludeskimdev@gmai.com>
2024-02-23 22:45:04 +0800

    Improve

--- script/commands/notifyadmin.js
+++ script/commands/notifyadmin.js
@@ -13,14 +13,14 @@ module.exports = {
     onRun: async function () {},
     onEvent: async function ({ api, event }) {
 		if (event.logMessageType === "log:subscribe") {
-            if (event.logMessageData.addedParticipants[0].userFbId === await api.getCurrentUserID()) {
+            const botUserID = await api.getCurrentUserID();
+            if (event.logMessageData.addedParticipants.some(participant => participant.userFbId === botUserID)) {
                 const threadInfo = await api.getThreadInfo(event.threadID);
                 const threadName = threadInfo.threadName;
                 const userInfo = await api.getUserInfo(event.author);
                 const msg = `==⚠ New Event ⚠==\nEvent: Added to a new group.\nGroup: ${threadName}\nAdded by: ${userInfo[event.author].name}`;
-                const fs = require("fs").promises;
-                const admin = await fs.readFile('config.json', "utf8");
-                JSON.parse(admin).admin.forEach(async (admin) => {
+                const admin = global.utils.loadConfig[0];
+                admin.admin.forEach(async (admin) => {
                     await api.sendMessage(msg, admin);
                 });
             }
