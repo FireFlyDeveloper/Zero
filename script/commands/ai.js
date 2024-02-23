07f1e5e19967621cd54755ebe2158a02a87dfc34
Kim Eduard Saludes <saludeskimdev@gmai.com>
2024-02-23 17:35:31 +0800

    Improved AI

--- script/commands/ai.js
+++ script/commands/ai.js
@@ -1,32 +1,48 @@
 const axios = require('axios');
+const Prefixes = [
+    'Ai',
+    'ai',
+    'ask',
+    'Ask',
+    '+ai',
+    'AI',
+    '+Ai'
+];
 
 module.exports = {
-  config: {
-    name: "ai",
-    alias: ["ai"],
-    version: 2.0,
-    author: "RazihelX",
-    role: 0,
-    countdown: 5,
-    description: "Ai ZERO",
-    category: "Artificial I",
-    guide: "Ai or multiprefix"
-  },
-  onRun: async function ({ api, event, args}) {
-    try {
-        const question = args.join(' ');
-        if (!question)
-            return await api.sendMessage(`please input a keyword or what to ask! `, event.threadID);
-        else {
-        
-            const message = await api.sendMessage("Thinking...", event.threadID, event.messageID);
-            const response = await axios.get(`https://openaikey.onrender.com/api?prompt=${encodeURIComponent(question)}`);
+    config: {
+        name: "ai",
+        alias: ["ai"],
+        version: 2.0,
+        author: "RazihelX",
+        role: 0,
+        countdown: 5,
+        description: "Ai ZERO",
+        category: "Artificial I",
+        guide: "Ai or multiprefix"
+    },
+    onRun: async function () {},
+    onMessage: async function ({ api, event }) {
+        try {
+            const prefix = Prefixes.find((p) => event.body && event.body.toLowerCase().startsWith(p));
+            if (!prefix) {
+                return;
+            }
+            const question = event.body.substring(prefix.length).trim();
+            if (question === "") {
+                await api.sendMessage(`please input a keyword or what to ask! `, event.threadID);
+                return;
+            }
+            
+            const info = await api.getUserInfo(event.senderID);
+            const name = info[event.senderID].name;
+            const message = await api.sendMessage("Thinking...💭", event.threadID, event.messageID);
+            const response = await axios.get(`https://openaikey.onrender.com/api?prompt=${encodeURIComponent(`User name: ${name}. User prompt:${question}`)}`);
             const messageText = response.data.response.trim();
-            return await api.editMessage(messageText, message.messageID);
+            await api.editMessage(messageText, message.messageID);
+        } catch (error) {
+            console.error("Error in AI command:", error.message);
+            await api.sendMessage("An error occurred while processing your request.", event.threadID);
         }
-    } catch (error) {
-        console.error("Error in AI command:", error.message);
-        return await api.sendMessage("An error occurred while processing your request.", event.threadID);
     }
-  }
 };
